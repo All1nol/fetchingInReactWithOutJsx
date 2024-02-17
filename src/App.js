e190b35a94d92f5e8a205cd21c792df925487136
omarmdv <newacckim@gmail.com>
2024-02-17 10:55:19 +0400

    AddedOnClick

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
-import React, {useEffect,useState} from 'react';
-import { ReactDOM, createElement } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const PokeComponent = () => {
   const [pokemonData, setPokemonData] = useState(null);
 
-const getRandomPokemonId = () => {
-  return Math.floor(Math.random() * 10 )+1;
-}
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const randomPokemonId= getRandomPokemonId();
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`);
-        if (response.ok) {
-          const data = await response.json();
-          
-          setPokemonData(data);
-        } else {
-          console.error('Failed to fetch data');
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
+  const getRandomPokemonId = () => {
+    return Math.floor(Math.random() * 10) + 1;
+  };
+
+  const fetchData = async () => {
+    try {
+      const randomPokemonId = getRandomPokemonId();
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`);
+      
+      if (response.ok) {
+        const data = await response.json();
+        setPokemonData(data);
+      } else {
+        console.error('Failed to fetch data');
       }
-    };
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
     fetchData();
   }, []);
 
-  const divElement = createElement('div', null,
-    createElement('h1', null, 'Pokemon Data:'),
-    pokemonData && React.createElement('h1', null, pokemonData.name),
-    pokemonData && React.createElement('img', {src: pokemonData.sprites.front_default, alt: 'Pokemon image'})
+  const renderClick = () => {
+    fetchData();
+  }
+
+  const divElement = (
+    <div>
+      <h1>Pokemon Data:</h1>
+      {pokemonData && <h1>{pokemonData.name}</h1>}
+      {pokemonData && <img onClick={renderClick} src={pokemonData.sprites.front_default} alt="Pokemon image" />}
+    </div>
   );
+
   return divElement;
 };
+
 export default PokeComponent;
