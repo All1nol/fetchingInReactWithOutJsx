5f7a907826775ad2d41f98a410ae79373a78b8af
omarmdv <newacckim@gmail.com>
2024-02-20 16:26:21 +0400

    hashing plus fetching according to PokemonID

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from 'react';
 
 const PokeComponent = () => {
-  const [pokemonData, setPokemonData] = useState(null);
-
   const getRandomPokemonId = () => {
     return Math.floor(Math.random() * 10) + 1;
   };
 
+  const [pokemonData, setPokemonData] = useState(null);
+  const [pokemonId, setPokemonId] = useState(getRandomPokemonId());
+
+  const generateId = () => {
+    const newPokemonId = getRandomPokemonId();
+    setPokemonId(newPokemonId);
+    window.location.hash = `#${newPokemonId}`;
+    fetchData();
+  };
+
   const fetchData = async () => {
     try {
-      const randomPokemonId = getRandomPokemonId();
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`);
-      
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+
       if (response.ok) {
         const data = await response.json();
         setPokemonData(data);
@@ -24,22 +31,24 @@ const PokeComponent = () => {
   };
 
   useEffect(() => {
-    fetchData();
+    const hashed = window.location.hash.substring(1);
+    if (hashed) {
+      setPokemonId(parseInt(hashed,10));
+      fetchData();
+    }
   }, []);
 
-  const renderClick = () => {
-    fetchData();
-  }
-
-  const divElement = (
+  return (
     <div>
       <h1>Pokemon Data:</h1>
-      {pokemonData && <h1>{pokemonData.name}</h1>}
-      {pokemonData && <img onClick={renderClick} src={pokemonData.sprites.front_default} alt="Pokemon image" />}
+      {pokemonData && (
+        <div>
+          <h1>{pokemonData.name}</h1>
+          <img onClick={generateId} src={pokemonData.sprites.front_default} alt="Pokemon image" />
+        </div>
+      )}
     </div>
   );
-
-  return divElement;
 };
 
 export default PokeComponent;
